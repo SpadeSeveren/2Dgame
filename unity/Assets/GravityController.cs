23eb49dd6d5d149e60ad17570daeb3600f7af766
Ryan Fleury <ryan.j.fleury@gmail.com>
2018-02-14 11:43:24 -0700

    Added gravity changing script and player movement

---
+++ unity/Assets/GravityController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour {
+    public float angle = 0,
+                 target_angle = 180;
+
+	// Use this for initialization
+	void Start () {
+        target_angle = 180; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		angle += (target_angle - angle) * 0.5f;
+	    Physics2D.gravity = new Vector2((float)Math.Cos((angle-90) * (Math.PI / 180.0))*100, (float)Math.Sin((angle-90) * (Math.PI / 180.0))*100);
+    }
+}
