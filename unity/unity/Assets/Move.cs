4610e8b217af8130af6aa5db28cfc193c539d61a
Ryan Fleury <ryan.j.fleury@gmail.com>
2018-02-19 12:08:04 -0700

    Made camera changes

---
+++ unity/unity/Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D body;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move_horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(Mathf.Cos(GravityController.angle * (Mathf.PI/180))*move_horizontal, 
+                                       Mathf.Sin(GravityController.angle * (Mathf.PI/180))*move_horizontal);
+        body.velocity += (movement * speed);
+    }
+}
